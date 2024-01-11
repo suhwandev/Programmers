7cd3f52f5e3f7dcacec2fb4b5bf9b821d690605a
류수환 <118111075+kkomijipsa@users.noreply.github.com>
2024-01-11 23:33:41 +0900

    [Bronze V] Title: Hello World, Time: 116 ms, Memory: 13852 KB -BaekjoonHub

---
+++ 백준/Bronze/2557. Hello World/Hello World.java
@@ -0,0 +1,7 @@
+public class Main {
+  public static void main(String[] args) {
+    StringBuilder builder = new StringBuilder();
+    builder.append("Hello World!");
+    System.out.println(builder);
+  }
+}
